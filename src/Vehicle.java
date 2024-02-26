f0a0c59dcf75d95151711267ce1b0d09608841a8
kiril <penzaringo2010@yandex.ru>
2024-02-26 23:15:53 +0300

    Сделано ДЗ

---
+++ src/Vehicle.java
@@ -0,0 +1,18 @@
+public abstract class Vehicle implements Maintenance {
+    private String modelName;
+    private int wheelsCount;
+    public Vehicle (String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+    @Override
+    public void check() {
+        System.out.println("Обслуживаем " + modelName);
+        for (int i = 0; i < wheelsCount; i++) {
+            updateTyre();
+        }
+    }
+}
